8775f5f9310399b779855c6b30de511b61a6de03
ShuyuWeng <wingy@apps.ntpc.edu.tw>
2023-04-28 18:51:07 +0800

    分數運算加減乘除

---
+++ ConsoleApp1/FractionOperations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FractionOperations
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+
+        public FractionOperations(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static FractionOperations Add(FractionOperations f1, FractionOperations f2)
+        {
+            int numerator = f1.Numerator * f1.Denominator + f2.Numerator * f2.Denominator;
+            int denominator = f1.Denominator * f2.Denominator;
+            return new FractionOperations(numerator, denominator);
+        }
+
+        public static FractionOperations Subtract(FractionOperations f1, FractionOperations f2)
+        {
+            int numerator = f1.Numerator * f1.Denominator - f2.Numerator * f2.Denominator;
+            int denominator = f1.Denominator * f2.Denominator;
+            int gcd = GCD(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+            return new FractionOperations(numerator, denominator);
+        }
+
+        public static FractionOperations Multiply(FractionOperations f1, FractionOperations f2)
+        {
+            int numerator = f1.Numerator *  f2.Numerator ;
+            int denominator = f1.Denominator * f2.Denominator;
+            int gcd = GCD(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+            return new FractionOperations(numerator, denominator);
+        }
+
+        public static FractionOperations Divide(FractionOperations f1, FractionOperations f2)
+        {
+            int numerator = f1.Numerator * f2.Denominator;
+            int denominator = f1.Denominator * f2.Numerator;
+            int gcd = GCD(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+            return new FractionOperations(numerator, denominator);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            FractionOperations other = (FractionOperations)obj;
+            return (Numerator == other.Numerator && Denominator == other.Denominator);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 17;
+            hashCode = hashCode * 23 + Numerator.GetHashCode();
+            hashCode = hashCode * 23 + Denominator.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        private static int GCD(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                return numerator;
+            return GCD(denominator, numerator % denominator);
+        }
+    }
+}
---
+++ TestProject1/UnitTestFractionOperations.cs
@@ -0,0 +1,52 @@
+﻿using ConsoleApp1;
+using StudentGrades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class UnitTestFractionOperations
+    {
+        [Test]
+        public void TestFractionAddition()
+        {
+            FractionOperations f1 = new FractionOperations(1, 2);
+            FractionOperations f2 = new FractionOperations(1, 3);
+            FractionOperations expected = new FractionOperations(5, 6);
+            FractionOperations result = FractionOperations.Add(f1, f2);
+            Assert.That(result.Numerator, Is.EqualTo(expected.Numerator));
+            Assert.That(result.Denominator, Is.EqualTo(expected.Denominator));
+        }
+
+        [Test]
+        public void TestFractionSubtraction()
+        {
+            FractionOperations f1 = new FractionOperations(3, 4);
+            FractionOperations f2 = new FractionOperations(1, 4);
+            FractionOperations result = FractionOperations.Subtract(f1, f2);
+            Assert.That(result,Is.EqualTo(new FractionOperations(1,2)));
+        }
+
+        [Test]
+        public void TestFractionMultiplication()
+        {
+            FractionOperations f1 = new FractionOperations(2, 3);
+            FractionOperations f2 = new FractionOperations(3, 5);
+            FractionOperations result = FractionOperations.Multiply(f1, f2);
+            Assert.That(result,Is.EqualTo(new FractionOperations(2, 5)));
+        }
+
+        [Test]
+        public void TestFractionDivision()
+        {
+            FractionOperations f1 = new FractionOperations(2, 3);
+            FractionOperations f2 = new FractionOperations(4, 5);
+            FractionOperations result = FractionOperations.Divide(f1, f2);
+            Assert.That(result,Is.EqualTo(new FractionOperations(5, 6)));
+        }
+    }
+}
